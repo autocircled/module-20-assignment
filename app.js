f8ee0784d214e477c33bbec8dbc4b028e748708b
Moktadir Rahman LP <autocircled@gmail.com>
2024-01-06 23:25:36 +0600

    git init

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser'); 
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const hpp = require('hpp');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: '50mb' , extended: true}));
+
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(hpp());
+
+//Request rate Limit
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 2000
+})
+app.use(limiter)
+
+//Database connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB Connected')
+}).catch((err) => {
+    console.log(err)
+})
+
+//Routing implementation
+const router = require('./src/routes/api');
+app.use("/api/v1", router);
+app.use('*', (req, res) => {
+    if (req.baseUrl === ''){
+        res.status(200).send('<h1 style="text-align:center">Module 18 Assignment</h1>')
+    }else{
+        res.status(404).send('<h1 style="text-align:center">404 Not Found<h1>')
+    }
+})
+
+
+module.exports = app ;
---
+++ src/routes/api.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+const router = Router()
+
+// const { createUser, updateUser, deleteUser, getUser } = require('../controllers/UserController')
+// router.post('/register', createUser)
+// router.put('/updateProfile/:id', updateUser)
+// router.delete('/deleteUser/:id', deleteUser)
+// router.get('/getUser', getUser)
+
+
+module.exports = router;
